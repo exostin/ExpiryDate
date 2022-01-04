58635c1abceaf9b8e9e3845861f332dc24135136
Damian Dorosz <doroszdam@gmail.com>
2022-01-04 22:54:43 +0100

    Make a working game screen, closed #3

---
+++ Assets/Scripts/GetVersion.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GetVersion : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI versionText;
+
+    private void Start()
+    {
+        versionText.text = "v" + Application.version;
+    }
+}
