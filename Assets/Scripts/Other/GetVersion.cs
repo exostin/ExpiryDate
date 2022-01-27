2c9a01f46115b12f110860670ea02a8a752a3275
Damian Dorosz <doroszdam@gmail.com>
2022-01-27 17:53:32 +0100

    Minor fixes

--- Assets/Scripts/Other/GetVersion.cs
+++ Assets/Scripts/Other/GetVersion.cs
@@ -1,12 +1,15 @@
 ﻿using TMPro;
 using UnityEngine;
 
-public class GetVersion : MonoBehaviour
+namespace Other
 {
-    [SerializeField] private TextMeshProUGUI versionText;
-
-    private void Start()
+    public class GetVersion : MonoBehaviour
     {
-        versionText.text = "v" + Application.version;
+        [SerializeField] private TextMeshProUGUI versionText;
+
+        private void Start()
+        {
+            versionText.text = "v" + Application.version;
+        }
     }
 }
