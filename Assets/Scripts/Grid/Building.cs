59afbd06a3626994a10eb96235666966d2b3d2b2
Damian Dorosz <doroszdam@gmail.com>
2022-02-19 23:46:21 +0100

    Day1

---
+++ Assets/Scripts/Grid/Building.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Grid
+{
+    public class Building : MonoBehaviour
+    {
+        public bool Placed { get; private set; }
+        public BoundsInt area;
+        void Start()
+        {
+            
+        }
+
+        #region Build Methods
+
+    
+
+        #endregion
+    }
+}
