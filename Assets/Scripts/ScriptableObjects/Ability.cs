88ecd2717933784d13123fcd59ab47ab0b7622dd
Damian Dorosz <doroszdam@gmail.com>
2022-04-13 23:41:13 +0200

    Add more statistics to scriptable objects

--- Assets/Scripts/ScriptableObjects/Ability.cs
+++ Assets/Scripts/ScriptableObjects/Ability.cs
@@ -5,13 +5,14 @@ namespace ScriptableObjects
     [CreateAssetMenu(fileName = "New ability", menuName = "Ability")]
     public class Ability : ScriptableObject
     {
-        public string abilityName;
-        public int damage;
-        public string description;
+        public string abilityName = "ability name";
+        public string description = "ability description";
+        public int damage = 100;
+        public int targetCount = 1;
+        public bool canOnlyTargetAllies = false;
+        public bool usedOnSelf = false;
+        public bool buff = false;
         public Sprite artwork;
         public AudioClip soundEffect;
-
-        public bool canOnlyTargetAllies;
-        //public bool usedOnSelf;
     }
 }
