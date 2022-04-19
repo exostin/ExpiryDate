783cbc2764917c8c5d7e1ef089f83d7a2196c80f
kubagp <kubagp2@gmail.com>
2022-04-20 00:35:40 +0200

    Load defenders costs from scriptable objects

--- Assets/Scripts/Controllers/GameManager.cs
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
+using Classes;
 using Classes.Citybuilding;
+using ScriptableObjects;
 using UnityEngine;
 using Resources = UnityEngine.Resources;
 
@@ -12,6 +15,12 @@ namespace Controllers
 
         [SerializeField] private GameObject pauseMenu;
 
+        [SerializeField] private Character fighterCharacter;
+        [SerializeField] private Character shooterCharacter;
+        [SerializeField] private Character medicCharacter;
+        [SerializeField] private Character robotCharacter;
+        [SerializeField] private Character droneCharacter;
+
         public Manager cbm;
 
         private void Start()
@@ -21,7 +30,32 @@ namespace Controllers
                 .GetComponent<SettingsMenuController>()
                 .Initialize();
             stateController = GameObject.FindGameObjectWithTag("StateController").GetComponent<StateController>();
+
             cbm = new Manager();
+
+            #region Copy costs of defenders from scriptable objects to cbm
+
+            var characterDefenders = new Dictionary<Character, Defender>
+            {
+                {fighterCharacter, cbm.Defenders[DefenderType.Fighter]},
+                {shooterCharacter, cbm.Defenders[DefenderType.Shooter]},
+                {medicCharacter, cbm.Defenders[DefenderType.Medic]},
+                {robotCharacter, cbm.Defenders[DefenderType.Robot]},
+                {droneCharacter, cbm.Defenders[DefenderType.Drone]}
+            };
+
+            foreach (var (character, defender) in characterDefenders)
+                defender.BaseCost = new Classes.Citybuilding.Resources
+                {
+                    Energy = character.costEnergy,
+                    Food = character.costFood,
+                    Titan = character.costTitan,
+                    Water = character.costWater
+                };
+
+            #endregion
+
+            // TODO: Copy costs from excel, make ui for buying defenders, link scriptable objects to gm in unity
         }
 
         public void TogglePauseMenu()
